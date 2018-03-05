c13b20b3af5efbb6bd76222e0da8c9f125fbccb9
Family-TreeSY <286210002@qq.com>
2018-03-05 12:20:13 +0800

    '增加开发环境'

--- weblog/manage.py
+++ weblog/manage.py
@@ -3,8 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
+    profile = os.environ.get('WEBLOG_PROFILE', 'develop')
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "weblog.settings")
+        "DJANGO_SETTINGS_MODULE", "weblog.settings.%s" % profile)
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
