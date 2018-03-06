b8eef434491b708b2797ea01041f4bf659c3634f
Family-TreeSY <286210002@qq.com>
2018-03-06 18:04:46 +0800

    '增加友链'

--- weblog/config/views.py
+++ weblog/config/views.py
@@ -1,3 +1,15 @@
+# -*- coding:utf-8 -*-
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+
+from blog.views import CommonMixin
+from .models import Link
+
+
+class LinkView(CommonMixin, ListView):
+    queryset = Link.objects.filter(status=1)
+    model = Link
+    template_name = 'config/link.html'
+    context_object_name = 'links'
+
